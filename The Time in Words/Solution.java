bbf6578c20b52b7bd649db5e70f085ef858efbf3
Tugu8 <tugutugu018@gmail.com>
2025-06-03 21:47:56 +0800

    Add files via upload

---
+++ The Time in Words/Solution.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'timeInWords' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER h
+     *  2. INTEGER m
+     */
+
+    public static String timeInWords(int h, int m) {
+    // Write your code here
+        String[] numbers = {
+            "zero", "one", "two", "three", "four", 
+            "five", "six", "seven", "eight", "nine",
+            "ten", "eleven", "twelve", "thirteen",
+            "fourteen", "fifteen", "sixteen", 
+            "seventeen", "eighteen", "nineteen",
+            "twenty", "twenty one", "twenty two",
+            "twenty three", "twenty four", "twenty five",
+            "twenty six", "twenty seven", "twenty eight",
+            "twenty nine"
+        };
+        
+        if (m == 0) {
+            return numbers[h] + " o' clock";
+        } else if (m == 1) {
+            return "one minute past " + numbers[h];
+        } else if (m == 15) {
+            return "quarter past " + numbers[h];
+        } else if (m == 30) {
+            return "half past " + numbers[h];
+        } else if (m == 45) {
+            return "quarter to " + numbers[h % 12 + 1];
+        } else if (m < 30) {
+            return numbers[m] + " minutes past " + numbers[h];
+        } else {
+            return numbers[60 - m] + " minutes to " + numbers[h % 12 + 1];
+        }
+    }
+    }
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int h = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.timeInWords(h, m);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
